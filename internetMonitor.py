c515f7f26bd30ce040b0d37df3b840efb96ee383
pedroigosl <pedroigosl@gmail.com>
2022-05-18 16:05:34 -0300

    internet monitor completely unrelated to the project. Need to remake it...

---
+++ internetMonitor.py
@@ -0,0 +1,22 @@
+from termcolor import colored
+import requests
+import time
+
+url = "https://www.google.com"
+timeout = 5
+LINE_CLEAR = '\x1b[2K' # <-- ANSI sequence
+
+toggle = True
+start = time.time()
+while(True):
+    print(end=LINE_CLEAR) # <-- clear the line where cursor is located
+    try:
+        end = time.time()
+        if not toggle:
+            start = time.time()
+        request = requests.get(url, timeout=timeout)
+        print(colored(f"connected - {end - start}", "green"),end = "\r")
+        toggle = True
+    except (requests.ConnectionError,requests.Timeout) as exception:
+        print(colored("not connected", "red"),end = "\r")
+        toggle = False
